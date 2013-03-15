7253d9a0c148bd08d57bb9c89ff401c7edf2c824
egoing <egoing@ubuntu.(none)>
2013-03-15 08:20:08 -0700

    view 예제 추가

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -1,30 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Topic extends CI_Controller {
-	function index(){
-		echo '
-		<!DOCTYPE html>
-		<html>
-			<head>
-				<meta charset="utf-8"/>
-			</head>
-			<body>
-				토픽 페이지
-			</body>
-		</html>
-		';
-	}
-	function get($id){
-		echo '
-		<!DOCTYPE html>
-		<html>
-			<head>
-				<meta charset="utf-8"/>
-			</head>
-			<body>
-				토픽 '.$id.'
-			</body>
-		</html>
-		';
-	}
+    function index(){
+        $this->load->view('head');
+        $this->load->view('main');
+        $this->load->view('footer');
+    }
+    function get($id){
+        $this->load->view('head');
+        $this->load->view('get', array('id'=>$id));
+        $this->load->view('footer');
+    }
 }
 ?>
